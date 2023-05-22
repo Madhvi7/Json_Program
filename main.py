887d012d6555bfbcd77edc3f0290f942f5295be9
Madhvi_Singh <89591576+Madhvi7@users.noreply.github.com>
2023-05-22 19:13:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+"""program to fetch the keys from the given json"""
+import json
+
+f = open('text.json')
+
+data = json.load(f)
+details1 = data['siteDetails']
+for i in details1:
+
+    print(i)
+
+f.close()
+
+data_set = []
+details2 = data['parametersList']
+for item in details2:
+    dictionary = {
+        "parameterName": item['parameterName'],
+        "min_value": item['max'],
+        "max_value": item['min'],
+        "avg_value": item['avg']
+    }
+    data_set.append(dictionary)
+print(data_set)
+json_output = json.dumps(data_set, indent=4)
+# Writing to solution json
+with open("solution.json", "w") as outfile:
+    outfile.write(json_output)
